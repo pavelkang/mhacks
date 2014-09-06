85a0ef7addfa6c29764164948b6f7a78e3451535
kai <sclingtt@gmail.com>
2014-09-06 12:31:00 -0400

    start student.js

--- static/js/student.js
+++ static/js/student.js
@@ -1,9 +1,29 @@
-var stuApp = angular.module('stuApp', ['ngAudio']);
+var stuApp = angular.module('stuApp', []);
+// factory goes here
+
 
 stuApp.controller('stuCtrl', function($scope, $http){
+    $scope.mp3source = "../static/audio/canon.mp3";
     $scope.form = {
         choice : 0,
-        input : ""
+        input : "",
+        type : -1,
+        ID : -1
     };
+    $scope.submit = function() {
+        if (validate($scope.form)) {
+            // http request here
+        }
+    }
+});
 
-})
+var validate = function(form) {
+    if (type==0) { // choice question
+    } else { // input question
+        if (input==="") {
+            alert("Not valid!");
+            return false;
+        }
+    }
+    return true;
+}
