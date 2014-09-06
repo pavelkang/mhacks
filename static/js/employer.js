6e2a07fbbdd35f876a9841b77ece73d340447093
kai <sclingtt@gmail.com>
2014-09-06 14:50:01 -0400

    getHist

--- static/js/employer.js
+++ static/js/employer.js
@@ -1,5 +1,21 @@
 var empApp = angular.module('empApp', []);
 
-empApp.controller('empCtrl', function($scope){
-    $scope.test = "emp";
+empApp.factory('empFactory', function($http, $q){
+    return {
+        getHist : function() {
+            var deferred = $q.defer();
+            $http.get('/api/hist').success(function(data){
+                deferred.resolve(data);
+            }).error(function(){
+                deferred.reject('An error occurred!')
+            });
+            return deferred.promise;
+        }
+    }
+});
+
+empApp.controller('empCtrl', function(empFactory, $scope){
+    empFactory.getHist().then(function(data){
+        console.log(data);
+    })
 })
